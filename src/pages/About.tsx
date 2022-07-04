f976e8e08d7f058cc8829b516a10af2b8d1ee38f
Thomas <tom.fisto@gmail.com>
2022-07-04 14:49:40 +0000

    Commit routes

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const About = () => {
+    return(
+        <div>
+            <h1>A propo s</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus quo quos ad! Beatae laboriosam in quidem asperiores, iusto, ratione tempore illum culpa, quam ducimus illo voluptatum deleniti magnam cumque!</p>
+        </div>
+    );
+};
+    
+    export default About;
